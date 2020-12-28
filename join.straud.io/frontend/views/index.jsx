0be8a36b153e890786065920bc6a643127491acd
Alex Olsen <aolsenjazz@gmail.com>
2020-12-28 14:01:07 -0500

    refacted join page to not destroy + recreate services on minor disruption

--- join.straud.io/frontend/views/index.jsx
+++ join.straud.io/frontend/views/index.jsx
@@ -1,83 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, Fragment } from 'react';
 import { Head } from '@react-ssr/express';
 
-import Header from '@Components/Header';
+import ServicesContainer from '@Components/ServicesContainer';
 import JoinApp from '@Components/JoinApp';
 
-import Api from '@Services/api';
-import SignalService from '@Services/signal-service';
-import PeerConnectionService from '@Services/peer-connection-service';
-import DefaultContext from '../default-context';
+import useBrowserCompat from '@Hooks/use-browser-compat';
 
 import './global.css';
-// import watchVisiblity from '../visibility';
-// import isMobile from 'ismobilejs';
-// import watch from '../disruption';
-// import compatCheckBrowser from '../compatibility'
-
-// import './join.css';
-
-// import Header from '../components/Header';
-// import JoinPanel from '../components/JoinPanel';
-// import RoomView from '../components/RoomView';
-// import TerminatedView from '../components/TerminatedView';
-// import BadBrowserView from '../components/BadBrowserView';
-// import LostInternetTooltip from '../components/LostInternetTooltip';
-// import LoadingAnim from '../components/LoadingAnim';
-
-import AudioPlayer from '../audio-player.js';
-// import AnalyticsPanel from '../components/AnalyticsPanel';
 
 export default function Home(props) {
-	const api = new Api(props.apiUrl);
-	const audioPlayer = new AudioPlayer();
-	const signalService = new SignalService(props.signalUrl);
-	const peerConnectionService = new PeerConnectionService(signalService, audioPlayer);
-
-	useEffect(() => {
-		signalService.connect();
-	}, []);
-
-	// ????????????????????????????
-	// useEffect(() => {
-	// 	let result = compatCheckBrowser();
-
-	// 	if (!result.valid) {
-	// 		this.updateState('error', {
-	// 			severity: 'browser',
-	// 			message: result.message,
-	// 		});
-	// 		return;
-	// 	}
-	// }, []);
-
-	// ??????????????????????????????
-	// useEffect(() => {
-	// 	watch(this.onDisruption);
-	// }, []);
-
-	// useEffect(() => {
-	// 	require('unmute-ios-audio')();
-	// });
-
-	// ??????????????????????????????
-	// useEffect(() => {
-	// 	let params = new URLSearchParams(window.location.search);
-	// 	if (params.get('analytics') !== null) {
-	// 		this.updateState('analytics', true);
-	// 	}
-	// })
+	const browserCompat = useBrowserCompat();
 
 	return (
-		<DefaultContext.Provider value={{
-			signalService: signalService, 
-			api: api, 
-			peerConnectionService: peerConnectionService,
-			audioPlayer: audioPlayer,
-		}}>
-			<Header showLogin={true} />
-
-			<JoinApp />
-		</DefaultContext.Provider>
+		<Fragment>
+			{ browserCompat.compat === true  ? 
+				<ServicesContainer {... props}>
+					<JoinApp />
+				</ServicesContainer> 
+				: 
+				null 
+			}
+			{ browserCompat.compat === false ? <h1>noncompat</h1> : null }
+		</Fragment>
 	);
 }
+
+
+		
